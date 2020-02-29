3684943478b26049a6f1b9dce9d894add65d82e9
M. Nurul Huda <nurul.huda259@gmail.com>
2020-02-29 11:12:51 +0700

    Candy CBT v27

---
+++ config/config.candy.php
@@ -0,0 +1,8 @@
+<?php
+$copyright = 'Candy CBT';
+
+define("VERSI", "2.7.0");
+define("REVISI", "0");
+define("APLIKASI", "Candy CBT");
+define("NAMA_DATABASE", "cbtcandy27");
+define("BASEPATH", "cbtcandy"); 
